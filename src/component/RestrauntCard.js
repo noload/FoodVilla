1a892bb6d9c95d3d32acbfe6e4053e44d23c0e60
noload <vaibhavkamble023@gmail.com>
2024-01-08 19:08:18 +0530

    added basic css

--- src/component/RestrauntCard.js
+++ src/component/RestrauntCard.js
@@ -3,20 +3,12 @@ import { baseURL } from "../config";
 const RestrauntCard = ({ name, cuisines, avgRating, cloudinaryImageId }) => {
   return (
     <>
-      <a
-        href="/"
-        style={{
-          textDecoration: "none",
-          color: "black",
-        }}
-      >
-        <div className="card">
-          <img src={baseURL + cloudinaryImageId} />
-          <h2>{name}</h2>
-          <h3>{cuisines.join(", ")} </h3>
-          <h4>{avgRating} stars</h4>
-        </div>
-      </a>
+      <div className="card">
+        <img src={baseURL + cloudinaryImageId} />
+        <h2>{name}</h2>
+        <h3>{cuisines.join(", ")} </h3>
+        <h4>{avgRating} stars</h4>
+      </div>
     </>
   );
 };
